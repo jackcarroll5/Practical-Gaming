86f7252264d1a1c420edd561f0c460f6d73f8bba
Jack Carroll <Jack.Carroll@students@ittralee.ie>
2018-02-24 12:55:18 +0000

    Contains the updated test version of the gobstopper collecting with a sound effect for collecting the first gobstopper. But, progress must be made for the collecting of the rest of the gobstoppers by making the rest of them disappear and allow another gobstopper to spawn after the same sound effect is made. Player clones to another player each time a gobstopper is collected but moves all over the place at the moment.

---
+++ Conga Line 3D V7/Assets/Scripts/HighScoreManager.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HighScoreManager : MonoBehaviour {
+
+    /// <summary>
+    /// Current Score of the game. Set to 0 at start
+    /// </summary>
+    private int highscore = 0;
+
+    ScoreManager finalScore;
+
+    /// <summary>
+    /// Text of Score to be placed at top right corner of screen.
+    /// </summary>
+    TextMesh highScoreText;
+
+    // Use this for initialization
+    void Start()
+    {
+        print("Starting");
+        highScoreText = GetComponent<TextMesh>();
+         highScoreText.text = "High Score: " + highscore;
+        highScoreText.transform.position = new Vector3(1.5f, 10, 0);
+        //highScoreText.transform.parent = Camera.main.transform;//Attaches score text to camera so it can be seen on screen.
+        DontDestroyOnLoad(gameObject);
+       
+    }
+
+    private void setHighScore()
+    {
+        finalScore = FindObjectOfType<ScoreManager>();
+
+        if (finalScore) print("found score");
+
+        if (finalScore.currentScore > highscore)
+        {
+            highscore = finalScore.currentScore;
+            print("High Score is: " + highscore);
+             highScoreText.text = "High Score: " + highscore;
+        }
+       
+        highScoreText.characterSize = 0.035f;
+        highScoreText.font = Resources.GetBuiltinResource(typeof(Font), "Arial.ttf") as Font;
+        highScoreText.fontSize = 230;
+
+        print(highscore);
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+        setHighScore();
+        PlayerPrefs.SetInt("highscore", highscore);
+    }
+}
