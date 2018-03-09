f6bed3cc02783c5f658a92d7ac7351706c5a9bcb
STUDENTS\T00194823 <Jack.Carroll@students.itralee.ie>
2018-03-09 10:57:59 +0000

    Changes to the player cloning where the cloned players sometimes are directly straight in the line and are moving in time with the camera.

---
+++ Conga Craze 3D V13/Assets/Scripts/GameManagerCon.cs
@@ -0,0 +1,238 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.SceneManagement;
+
+public class GameManagerCon : MonoBehaviour {
+
+    public enum ItemType { Gobstopper }
+
+    public Transform gobClone;
+    public Transform playerClone;
+
+    private List<FrontPersonInLine> players;
+    private List<PickUpGob> items;
+
+    WorldFormation world;
+    FrontPersonInLine congaExtends;
+    ScoreManager score;
+    PickUpGob sweet;
+
+    //private int noCollectedGobs;
+    private int nostartingGobs = 1;
+    private int points = 1;
+    private int minPoints = 1;
+    private int maxPoints = 65;
+
+    public AudioSource gulp;
+
+    public Color[] colours = new Color[5];
+
+
+    // Use this for initialization
+    void Start () {
+
+        world = FindObjectOfType<WorldFormation>();
+
+        players = new List<FrontPersonInLine>();
+         items = new List<PickUpGob>();
+
+        gulp = GetComponent<AudioSource>();
+
+        /* for (int i = 0; i < nostartingGobs; i++)
+             spawnRanGob();*/
+
+        
+
+        //playerSpawn();
+        Vector3 playerStartPos = world.randomEmptypos();
+        //Instantiate(playerClone, playerStartPos + 0.3f * Vector3.up,Quaternion.identity);
+
+        /*Vector3 itemPos = world.randomEmptypos();
+           theItemSpawn(itemPos);*/
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    /// <summary>
+    /// Method that triggers the end game if the player(Line) hits the wall or the line itself
+    /// </summary>
+    public void gameOver()
+    {
+        SceneManager.LoadScene("SetStart");
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    /// <summary>
+    /// Intended to spawn the player somewhere within the arena.
+    /// </summary>
+    public void playerSpawn()
+    {
+        //Vector3 posSpawn = new Vector3(1,1,1);
+        Vector3 posSpawn1 = new Vector3(UnityEngine.Random.Range(1, world.arenaWidth - 1), world.heightofArenaFloor, UnityEngine.Random.Range(1, world.arenaDepth - 1));
+        playerLineSpawnAt(posSpawn1);
+
+    }
+
+    /// <summary>
+    /// Method for destroying the conga line if front person hits the wall or the line itself
+    /// </summary>
+    public void destroyConga(FrontPersonInLine player)
+    {
+       
+    }
+
+    /// <summary>
+    /// Method to destroy the solo player if it hits the wall
+    /// </summary>
+    public void destroyPerson(FrontPersonInLine player)
+    {
+        players.Remove(player);
+    }
+
+    /// <summary>
+    /// Method to destroy the gobstopper item
+    /// </summary>
+    public void destroyItem(PickUpGob item)
+    {
+        world = FindObjectOfType<WorldFormation>();
+
+        congaExtends = FindObjectOfType<FrontPersonInLine>();
+
+        sweet = FindObjectOfType<PickUpGob>(); 
+
+        items.Remove(item);
+
+        addScore(); 
+
+        cloneNewPerson();
+
+        gobCloneSpawn();
+
+        gulp.Play(); //Creates the sound effect for gulping when the gobstopper is collected
+
+        // spawnRanGob();         
+    }
+
+ /// <summary>
+    /// Spawns gobstopper in a random section of the arena
+    /// </summary>
+    public void spawnRanGob()
+    {
+        ItemType newItemType = (ItemType)UnityEngine.Random.Range(0, 1);
+        Vector3 posSpawn = world.randomEmptypos();
+
+        itemSpawn(newItemType, posSpawn);
+
+        sweet.IAm(this);
+
+    }
+    public void gobCloneSpawn()
+    {
+        //for(int i = 0; i < maxGobstoppers; i++)
+        //{
+
+        Vector3 gobStopSpawn = new Vector3(UnityEngine.Random.Range(1, world.arenaWidth - 1), world.heightofArenaFloor + 1, UnityEngine.Random.Range(1, world.arenaDepth - 1));
+        //itemSpawn(ItemType.Gobstopper, gobStopSpawn);
+       // itemSpawn(newItemType, gobStopSpawn);
+        theItemSpawn(gobStopSpawn);
+       //sweet.ConfirmSweetIns(ItemType.Gobstopper, 1, 10f);
+        sweet.IAm(this);
+        //}
+
+       
+    }
+
+    public void cloneNewPerson()
+    {      
+          
+            //playerClone.transform.position = new Vector3(playerClone.position.x, playerClone.position.y, playerClone.position.z - 4);
+            playerLineSpawnAt(playerClone.transform.position);
+            congaExtends.changeCameraPos();
+    }
+   
+    public void addScore()
+    {
+         points = sweet.pts;
+        score = FindObjectOfType<ScoreManager>();
+        score.increaseScore(points);//Communicates with the ScoreManager to increase the points once the gobstopper collected.
+
+    }
+
+
+    public int pts
+    {
+        get
+        {
+            return points;
+        }
+        set
+        {
+            if (value >= maxPoints)
+            {
+                points = maxPoints;
+            }
+            else if (value <= minPoints)
+            {
+                points = minPoints;
+            }
+        }
+    }
+
+
+    /// <summary>
+    /// Method to spawn the gobstopper within the arena.
+    /// </summary>
+    public void itemSpawn(ItemType newItemType, Vector3 posSpawn)
+    {
+
+        Transform newItem = Instantiate(gobClone, posSpawn, Quaternion.identity);
+        PickUpGob newGobControl = newItem.GetComponent<PickUpGob>();
+        newGobControl = FindObjectOfType<PickUpGob>();
+
+
+        if (newGobControl)
+        {
+            print("FOUND SCRIPT");            
+            items.Add(newGobControl);
+        }
+        else print("No script");
+    }
+
+    public void theItemSpawn(Vector3 posSpawn)
+    {
+
+        Transform newItem = Instantiate(gobClone, posSpawn, Quaternion.identity);
+        PickUpGob newGobControl = newItem.GetComponent<PickUpGob>();
+        newGobControl = FindObjectOfType<PickUpGob>();
+
+        newGobControl.IAm(this);
+        items.Add(newGobControl);
+    }
+
+
+    /// <summary>
+    /// Possible method to spawn the player clone in order to make the player a parent of the front person in line
+    /// </summary>
+    public void playerLineSpawnAt(Vector3 posToSpawn)
+    {
+        Transform newPlayer = Instantiate(playerClone, posToSpawn, Quaternion.identity);
+        FrontPersonInLine newPlayerControl = newPlayer.GetComponent<FrontPersonInLine>();
+        newPlayerControl.confirmPlayer(this);
+
+        newPlayerControl = FindObjectOfType<FrontPersonInLine>();
+        newPlayer.gameObject.tag = "Clone";
+        newPlayer.transform.parent = playerClone.transform;
+
+
+
+
+        newPlayer.GetComponent<FrontPersonInLine>().enabled = false;      
+        players.Add(newPlayerControl);
+    }
+}
---
+++ Conga Craze 3D V13/Assets/Scripts/PickUpGob.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class PickUpGob : MonoBehaviour
+{
+
+    private int points = 1;
+    private int minPoints = 1;
+    private int maxPoints = 3;
+    private float growTime = 1.5f, growTimer = 0f;
+    private float startScale = 0.0f;
+    private float rotateSpeed = 360.0f;
+
+    itemState currentState = itemState.grow;
+
+    GameManagerCon managerCon;
+    ScoreManager scoreMan;
+
+
+
+    enum itemState { grow,destroy};
+
+    Transform part;
+
+    // Use this for initialization
+    void Start () {
+        growTimer = 0;
+        transform.localScale = startScale * Vector3.one;
+        managerCon = FindObjectOfType<GameManagerCon>();
+        scoreMan = FindObjectOfType<ScoreManager>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        transform.Rotate(Vector3.up, rotateSpeed * Time.deltaTime);
+
+        switch(currentState)//Switches state of gobstoppers when the item is collected. It has states of being destroyed and grows up when the other gobstopper is collected
+        {
+            case itemState.grow:
+                growUp();
+
+                break;
+
+            case itemState.destroy:
+                managerCon.destroyItem(this);
+                Destroy(gameObject);
+
+                break;
+        }
+		
+	}
+
+    public int pts//Sets up the points system. Generally increases the score by one
+    {
+        get
+        {
+          return points;//Returns one point for every gobstopper collected.
+        }
+        set
+        {
+            if(value >= maxPoints)
+            {
+                points = maxPoints;
+            }
+            else if(value <= minPoints)
+            {
+                points = minPoints;
+            }
+        }
+    }
+
+    /// <summary>
+    /// Method that allows the gobstopper to grow at the start of the game and every time a gobstopper spawns.
+    /// </summary>
+    private void growUp()
+    {
+        growTimer += Time.deltaTime;
+        transform.localScale = Mathf.Lerp(startScale, 1.5f, (growTimer / growTime)) * Vector3.one;
+    }
+
+    /// <summary>
+    /// Confirms that the gobstopper was collected when the player picks up the gobstopper
+    /// </summary>
+    
+
+    internal void IAm (GameManagerCon gameManager)
+    {
+        managerCon = gameManager;
+    }
+
+    /// <summary>
+    /// All the events that occur once a gobstopper is colllected by the player (Line)
+    /// </summary>
+    private void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.GetComponent<FrontPersonInLine>())
+            //if (col.gameObject.tag == "Player")
+            {
+            managerCon = FindObjectOfType<GameManagerCon>();
+            //scoreMan.increaseScore(pts); 
+            managerCon.destroyItem(this);  //Communicates with the manager to confirm that the item is destroyed      
+            Destroy(gameObject);
+        }
+    }
+}
