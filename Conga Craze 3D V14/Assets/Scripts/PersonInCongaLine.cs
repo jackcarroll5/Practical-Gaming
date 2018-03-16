4d813933c11dd940a70298771e95bc44e2748fb9
STUDENTS\T00194823 <Jack.Carroll@students.itralee.ie>
2018-03-16 10:58:16 +0000

    This version successfully spawns the conga line players who are behind the front person and are last in line which follows the front in terms of direction and rotation

---
+++ Conga Craze 3D V14/Assets/Scripts/PersonInCongaLine.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PersonInCongaLine : MonoBehaviour {
+
+    // Use this for initialization
+
+    GameObject personInFrontOfMe;
+    float positionVar = 0.1f;
+    private float rotationVar = 0.1f;
+
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        transform.position = Vector3.Lerp(transform.position, personInFrontOfMe.transform.position, positionVar);
+        transform.rotation = Quaternion.Slerp(transform.rotation, personInFrontOfMe.transform.rotation, rotationVar);
+
+
+		
+	}
+
+
+    public void joinTheLineBehindMe(GameObject person)
+    {
+
+        personInFrontOfMe = person;
+    }
+
+}
